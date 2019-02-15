43fa63c2a0399653be8d4ba0aa2774868362fa7f
Athesdrake <athesdrake@gmail.com>
2019-02-15 22:22:06 +0100

    added license

--- aiotfm/__init__.py
+++ aiotfm/__init__.py
@@ -3,4 +3,4 @@ from .packet import Packet
 from . import utils
 
 from .__version__ import __author__, __title__, __description__
-from .__version__ import __url__, __version__, __credits__
+from .__version__ import __url__, __version__, __credits__, __license__
