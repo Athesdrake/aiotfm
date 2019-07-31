43c41f87fb50a9bff8b98ea8ca8a6906a2df3258
Athesdrake <athesdrake@gmail.com>
2019-07-31 02:36:55 +0200

    shorten aiotfm.player.Player to aiotfm.Player

--- aiotfm/__init__.py
+++ aiotfm/__init__.py
@@ -2,7 +2,7 @@ from aiotfm import utils
 from aiotfm.client import Client
 from aiotfm.connection import Connection
 from aiotfm.packet import Packet
-from aiotfm.player import Profile, Stats
+from aiotfm.player import Player, Profile, Stats
 from aiotfm.tribe import *
 
 from aiotfm.__version__ import __author__, __title__, __description__
