5e4f6ed3cb65817733e153abdb6f158fecf38f26
Athes <athesdrake@gmail.com>
2019-12-23 15:17:26 +0100

    Fixed old packet parser

--- aiotfm/client.py
+++ aiotfm/client.py
@@ -100,8 +100,10 @@ class Client:
 		"""
 		CCC = packet.readCode()
 		if CCC==(1, 1): # Old packets
-			data = packet.readBytes().split(b'\x01')
-			oldCCC = tuple(data.pop(0)[:2])
+			oldCCC, *data = packet.readString().split(b'\x01')
+			data = list(map(bytes.decode, data))
+			oldCCC = tuple(oldCCC[:2])
+
 			self.dispatch('old_packet', connection, oldCCC, data)
 			return await self.handle_old_packet(connection, oldCCC, data)
 
