3eaa15f170c436d1220caae28118d745cd6d0c6a
Athesdrake <athesdrake@gmail.com>
2020-04-13 00:10:11 +0200

    fix lint error

--- aiotfm/utils/__init__.py
+++ aiotfm/utils/__init__.py
@@ -3,4 +3,4 @@ from aiotfm.utils.date import Date
 from aiotfm.utils.get_keys import get_keys, Keys
 from aiotfm.utils.locale import Translation, Locale
 
-__all__ = ['shakikoo', 'Date', 'get_keys', 'Translation', 'Locale']
+__all__ = ['shakikoo', 'Date', 'get_keys', 'Translation', 'Locale', 'Keys']
