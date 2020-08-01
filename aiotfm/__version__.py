6de8fdedaf9d08bb5fb006f8eb261dc7782d356b
Athes <athesdrake@gmail.com>
2020-08-01 20:34:47 +0200

    v1.3.7

--- aiotfm/__version__.py
+++ aiotfm/__version__.py
@@ -2,7 +2,7 @@ __title__ = 'aiotfm'
 __description__ = "An asynchronous event based client for Transformice."
 __url__ = 'https://github.com/Athesdrake/aiotfm'
 
-__version__ = '1.3.6'
+__version__ = '1.3.7'
 __author__ = 'Athesdrake'
 __credits__ = [
 	'Tocutoeltuco', 'https://github.com/Tocutoeltuco/transfromage',
