d6cb08c5f5185f921d999a91925d2a145c77c600
extremq <45830561+extremq@users.noreply.github.com>
2021-04-10 03:08:55 +0300

    Update client.py

--- aiotfm/client.py
+++ aiotfm/client.py
@@ -65,7 +65,7 @@ class Client:
 
 	def __init__(
 		self,
-		community: int = Community.en,
+		community: Union[Community, int] = Community.en,
 		auto_restart: bool = False,
 		bot_role: bool = False,
 		loop: Optional[asyncio.AbstractEventLoop] = None,
