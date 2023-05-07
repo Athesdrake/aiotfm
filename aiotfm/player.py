101a535ba8d64c8b661e917b28e9ca9510aeffe4
Seniru Pasan Indira <senirupasan@gmail.com>
2023-05-07 12:12:38 +0000

    Fix naming
    isonline -> isOnline

--- aiotfm/player.py
+++ aiotfm/player.py
@@ -230,7 +230,7 @@ class Profile:
 		for _ in range(packet.read8()):
 			self.orbs.add(packet.read8())
 
-		self.isonline = packet.readBool()
+		self.isOnline = packet.readBool()
 		self.adventurePoints = packet.read32()
 
 
